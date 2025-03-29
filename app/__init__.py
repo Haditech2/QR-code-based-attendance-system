dd80b6a96e2e2824698bff3d278d634f1bb5ae5b
haditech2 <lahadiademu7@gmail.com>
2025-03-29 16:02:18 +0100

    Updated app initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -32,38 +31,3 @@ def create_app(config_object):
 
 # Create the application instance
 app = create_app(config[os.getenv('FLASK_ENV', 'default')])
-=======
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_migrate import Migrate
-import os
-from config import config
-
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-migrate = Migrate()
-
-def create_app(config_object):
-    app = Flask(__name__)
-    app.config.from_object(config_object)
-
-    db.init_app(app)
-    login_manager.init_app(app)
-    migrate.init_app(app, db)
-
-    from app.routes import auth, events, attendance, main
-    app.register_blueprint(main.bp)
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(events.bp)
-    app.register_blueprint(attendance.bp)
-
-    with app.app_context():
-        db.create_all()
-
-    return app
-
-# Create the application instance
-app = create_app(config[os.getenv('FLASK_ENV', 'default')]) 
->>>>>>> 5ce8070 (Update app initialization for Gunicorn deployment)
